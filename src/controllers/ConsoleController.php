6390b380c4e5c578dd6240061c8f363691373e5e
floor12 <floor12@floor12.net>
2019-11-12 01:10:03 +0100

    New console commands added.

--- src/controllers/ConsoleController.php
+++ src/controllers/ConsoleController.php
@@ -8,11 +8,15 @@
 
 namespace floor12\backup\controllers;
 
+use ErrorException;
 use floor12\backup\logic\BackupCreate;
 use floor12\backup\logic\BackupRestore;
 use floor12\backup\models\Backup;
+use floor12\backup\models\BackupType;
 use Yii;
+use yii\base\InvalidConfigException;
 use yii\console\Controller;
+use yii\helpers\Console;
 
 /**
  * Floor12 backup module console controller
@@ -23,29 +27,52 @@ use yii\console\Controller;
 class ConsoleController extends Controller
 {
     /**
-     * To backup data run: ./yii  backup/console/resotore {backup_config_name}
+     * Pass config_id to this command to create new backup.
      *
      * @param string $config_id
-     * @throws \yii\base\InvalidConfigException
+     * @throws InvalidConfigException
      */
     public function actionBackup(string $config_id)
     {
         Yii::createObject(BackupCreate::class, [$config_id])->run();
+        $this->stdout('Backup created.' . PHP_EOL, Console::FG_GREEN);
     }
 
     /**
-     * To restore backup run: ./yii  backup/console/resotore {backup_config_name}
+     * Pass existing backup ID to this command to restore from backup.
      *
      * @param string $backup_id
-     * @throws \ErrorException
-     * @throws \yii\base\InvalidConfigException
+     * @throws ErrorException
+     * @throws InvalidConfigException
      */
     public function actionRestore(string $backup_id)
     {
         $model = Backup::findOne((int)$backup_id);
         if (!$model)
-            throw new \ErrorException('Backup not found.');
+            throw new ErrorException('Backup not found.');
 
         Yii::createObject(BackupRestore::class, [$model])->run();
+        $this->stdout('Backup restored.' . PHP_EOL, Console::FG_GREEN);
+    }
+
+    /**
+     * List all created backups.
+     *
+     * @throws ErrorException
+     * @throws InvalidConfigException
+     */
+    public function actionIndex()
+    {
+        $this->stderr('Backups not found.' . PHP_EOL, Console::FG_YELLOW);
+
+        $models = Backup::find()->orderBy('id DESC')->all();
+        if (empty($models))
+            $this->stderr('Backups not found.' . PHP_EOL, Console::FG_YELLOW);
+
+        foreach ($models as $model)
+            $this->stdout("{$model->id}: " . Yii::$app->formatter->asDatetime($model->date) . "\t\t{$model->config_id}\t\t" .
+                BackupType::$list[$model->type] .
+                PHP_EOL,
+                $model->status ? Console::FG_GREEN : Console::FG_RED);
     }
 }
